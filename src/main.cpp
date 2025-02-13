42586ada226c05393b462e8ff490eefeaa3cfe94
eugenekingfish <aojg@protonmail.ch>
2025-02-13 23:00:27 +0000

    removed more unnecessary variables

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@
 #include <vector>
 #include <array>
 
-const int SCREEN_WIDTH = 1000; 
-const int SCREEN_HEIGHT = 1000;
+const int SCREEN_WIDTH = 1920; 
+const int SCREEN_HEIGHT = 1080;
 
 const int TOTAL_CIRCLES = 6;
 const int TOTAL_LINES = ( TOTAL_CIRCLES * (TOTAL_CIRCLES - 1) ) / 2; // Total number of edges in the complete graph on TOTAL_CIRCLES vertices
@@ -15,12 +15,6 @@ const int TOTAL_LINES = ( TOTAL_CIRCLES * (TOTAL_CIRCLES - 1) ) / 2; // Total nu
 const float GROWTH_MULT = 1.3f;
 
 std::vector<circle> circles;
-circle* source = nullptr;
-circle* target = nullptr;
-circle* lines[TOTAL_LINES][2];
-int line_counter = 0;
-circle* draggable_circle = nullptr;
-Vector2 circle_initial_positions[6];
 std::vector<Color> player_colors = {RED, BLUE};
 
 int main(void)
