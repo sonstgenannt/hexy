fdea75a6955966c2a05e4afd2fe251dc3437cd34
eugenekingfish <aojg@protonmail.ch>
2025-03-05 21:05:49 +0000

    updated rect_button

--- headers/rect_button.h
+++ headers/rect_button.h
@@ -9,13 +9,21 @@ class rect_button
       Rectangle bounds;
       Vector2 position;
       Color background_color = WHITE;
+      Color hover_background_color = WHITE;
+      Color current_color = WHITE;
       Color texture_color;
       Texture2D icon_texture;
+
+      bool mouse_over = false;
+      bool activated = false;
 
    public:
       rect_button(const Rectangle& bounds, const Vector2& position, const Color& background_color, const Color& texture_color, Texture2D& icon_texture);
       void draw() const;
+      void update();
 
+      void set_hover_background_color(const Color& color);
+      bool get_activated() const;
 };
 
 #endif
--- src/rect_button.cpp
+++ src/rect_button.cpp
@@ -11,6 +11,32 @@ rect_button::rect_button(const Rectangle& bounds, const Vector2& position, const
 
 void rect_button::draw() const
 {
-   DrawRectangleRec(this->bounds, this->background_color);
+   DrawRectangleRec(this->bounds, this->current_color);
    DrawTexture(this->icon_texture, position.x, position.y, this->texture_color);
+}
+
+void rect_button::set_hover_background_color(const Color& color)
+{
+   this->hover_background_color = color;
+}
+void rect_button::update()
+{
+   this->mouse_over = CheckCollisionPointRec(GetMousePosition(), this->bounds);
+
+   if ( mouse_over )
+   {
+      this->current_color = hover_background_color;
+
+      if ( IsMouseButtonPressed(0) )
+         this->activated = true;
+      else
+         this->activated = false;
+   }
+   else
+      this->current_color = background_color;
+}
+
+bool rect_button::get_activated() const
+{
+   return this->activated;
 }
