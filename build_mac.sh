8136bfda279f25b6615833a235889035ebcf0c07
eugenekingfish <aojg@protonmail.ch>
2025-03-07 17:02:48 +0000

    removed build_mac.sh

--- build_mac.sh
+++
@@ -1,1 +0,0 @@
-g++ -std=c++20 -Os -flto -I./include -o takaku_v0.05a src/rect_button.cpp src/entity.cpp src/line.cpp src/circle.cpp src/board.cpp src/ai.cpp src/data_manager.cpp src/main.cpp -L. -lraylib
