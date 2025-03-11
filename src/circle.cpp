0342c1ef886e8c4d72292489d310762e61a9d8a9
eugenekingfish <aojg@protonmail.ch>
2025-03-11 11:32:26 +0000

    formatting in circle.cpp

--- src/circle.cpp
+++ src/circle.cpp
@@ -1,60 +1,74 @@
 #include "../headers/circle.h"
 
-circle::circle(Vector2 position, float radius) : entity(position) {
+circle::circle(Vector2 position, float radius) : entity(position) 
+{
    this->initial_radius = radius;
    this->current_radius = radius;
 }
 
 circle::circle() {}
 
-void circle::set_initial_radius(float radius) {
+void circle::set_initial_radius(float radius) 
+{
    this->initial_radius = radius;
 }
 
-float circle::get_initial_radius() const {
+float circle::get_initial_radius() const 
+{
    return this->initial_radius;
 }
 
-void circle::set_current_radius(float radius) {
+void circle::set_current_radius(float radius) 
+{
    this->current_radius = radius;
 }
 
-float circle::get_current_radius() const {
+float circle::get_current_radius() const 
+{
    return this->current_radius;
 }
 
-void circle::set_mouse_over_growth_mult(float mult) {
+void circle::set_mouse_over_growth_mult(float mult) 
+{
    this->mouse_over_growth_mult = mult;
 }
 
-float circle::get_mouse_over_growth_mult() const {
+float circle::get_mouse_over_growth_mult() const 
+{
    return this->mouse_over_growth_mult;
 }
 
-void circle::add_outgoing_line(line* outgoing_line) {
+void circle::add_outgoing_line(line* outgoing_line) 
+{
    this->outgoing_lines.push_back(outgoing_line);
 }
 
-std::vector<line*> circle::get_outgoing_lines() const {
+std::vector<line*> circle::get_outgoing_lines() const 
+{
    return this->outgoing_lines;
 }
 
-void circle::kill_outgoing_lines() {
+void circle::kill_outgoing_lines() 
+{
    this->outgoing_lines.clear();
 }
 
-bool circle::is_mouse_over() const {
+bool circle::is_mouse_over() const 
+{
    return CheckCollisionPointCircle(GetMousePosition(), get_position(), this->current_radius);
 }
 
-void circle::set_frozen(bool b) {
+void circle::set_frozen(bool b) 
+{
    this->frozen = b;
 }
 
-bool circle::is_frozen() const {
+bool circle::is_frozen() const 
+{
    return this->frozen;
 }
 
-void circle::draw() {
+void circle::draw() 
+{
    DrawCircle(get_position().x, get_position().y, this->current_radius, get_color());
 }
