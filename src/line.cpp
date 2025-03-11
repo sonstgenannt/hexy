a46050b335bc064bc8d3471811a80a11c0070a4a
eugenekingfish <aojg@protonmail.ch>
2025-03-11 11:30:49 +0000

    formatting in line.cpp

--- src/line.cpp
+++ src/line.cpp
@@ -1,6 +1,7 @@
 #include "../headers/line.h"
 
-line::line(circle* source, circle* target) {
+line::line(circle* source, circle* target) 
+{
    this->source = source;
    this->target = target;
    source->add_outgoing_line(this);
@@ -9,7 +10,8 @@ line::line(circle* source, circle* target) {
    this->color = BLACK;
 }
 
-line::line(circle* source, circle* target, const Color& color) {
+line::line(circle* source, circle* target, const Color& color) 
+{
    this->source = source;
    this->target = target;
    source->add_outgoing_line(this);
@@ -18,7 +20,8 @@ line::line(circle* source, circle* target, const Color& color) {
    this->color = color;
 }
 
-line::line(circle* source, circle* target, const float& thickness, const Color& color) {
+line::line(circle* source, circle* target, const float& thickness, const Color& color) 
+{
    this->source = source;
    this->target = target;
    source->add_outgoing_line(this);
@@ -27,34 +30,43 @@ line::line(circle* source, circle* target, const float& thickness, const Color& 
    this->color = color;
 }
 
-circle* line::get_source() const {
+circle* line::get_source() const 
+{
    return this->source;
 }
-void line::set_source(circle* source) {
+void line::set_source(circle* source) 
+{
    this->source = source;
 }
 
-circle* line::get_target() const { 
+circle* line::get_target() const 
+{ 
    return this->target;
 }
-void line::set_target(circle* target) {
+void line::set_target(circle* target) 
+{
    this->target = target;
 }
 
-Color line::get_color() const {
+Color line::get_color() const 
+{
    return this->color;
 }
-void line::set_color(const Color& color) {
+void line::set_color(const Color& color) 
+{
    this->color = color;
 }
 
-void line::set_thickness(const float& thickness) {
+void line::set_thickness(const float& thickness) 
+{
    this->thickness = thickness;
 }
-float line::get_thickness() const {
+float line::get_thickness() const 
+{
    return this->thickness;
 }
 
-void line::draw() const {
+void line::draw() const 
+{
    DrawLineEx(this->source->get_position(), this->target->get_position(), this->thickness, this->color);
 }
